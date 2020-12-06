bd80aeeb51a4940175082e440652868c402ae0de
Valery Kovalev <robinsout@gmail.com>
2020-12-06 07:44:48 +0300

    lines svg moved to separate component

--- src/components/MoscowMap.vue
+++ src/components/MoscowMap.vue
@@ -12,303 +12,17 @@
     > -->
     <svg
       viewBox="-200 -50 1989.29 2104.19">
-
+      <moscow-lines/>
       <g
-        id="_5_line"
-        data-name="5_line">
-        <circle
-          class="cls-1"
-          cx="851.1"
-          cy="820.63"
-          r="375.62"/>
+        id="stations"
+        data-name="stations">
+        <station-mark
+          v-for="station in stationsGraph"
+          :stationData="getStationData(station.id)"
+          :key="station.id"
+          @click.native="handleClick(station.id)"/>
       </g>
-      <g
-        id="_7_line"
-        data-name="7_line">
-        <line
-          id="_7_line_21"
-          data-name="7_line_21"
-          class="cls-3"
-          x1="1578.97"
-          y1="1511.16"
-          x2="1578.97"
-          y2="1568.51"/>
-        <line
-          id="_7_line_20"
-          data-name="7_line_20"
-          class="cls-3"
-          x1="1578.97"
-          y1="1451.65"
-          x2="1578.97"
-          y2="1511.16"/>
-        <line
-          id="_7_line_19"
-          data-name="7_line_19"
-          class="cls-3"
-          x1="1578.97"
-          y1="1411.99"
-          x2="1578.97"
-          y2="1451.65"/>
-        <path
-          id="_7_line_18"
-          data-name="7_line_18"
-          class="cls-3"
-          d="M1813.36,1697.25c-.92,12.65,0,55.82,0,55.82"
-          transform="translate(-234.39 -341.08)"/>
-        <path
-          id="_7_line_17"
-          data-name="7_line_17"
-          class="cls-3"
-          d="M1797.63,1647.63c17.51,18.1,16.65,37,15.73,49.62"
-          transform="translate(-234.39 -341.08)"/>
-        <line
-          id="_7_line_16"
-          data-name="7_line_16"
-          class="cls-3"
-          x1="1464.24"
-          y1="1208.62"
-          x2="1563.24"
-          y2="1306.55"/>
-        <line
-          id="_7_line_15"
-          data-name="7_line_15"
-          class="cls-3"
-          x1="1416.56"
-          y1="1161.12"
-          x2="1464.24"
-          y2="1208.62"/>
-        <line
-          id="_7_line_14"
-          data-name="7_line_14"
-          class="cls-3"
-          x1="1326.09"
-          y1="1070.09"
-          x2="1416.56"
-          y2="1161.12"/>
-        <line
-          id="_7_line_13"
-          data-name="7_line_13"
-          class="cls-3"
-          x1="1210.92"
-          y1="955.91"
-          x2="1326.09"
-          y2="1070.09"/>
-        <line
-          id="_7_line_12"
-          data-name="7_line_12"
-          class="cls-3"
-          x1="1070.41"
-          y1="814.85"
-          x2="1210.92"
-          y2="955.91"/>
-        <path
-          id="_7_line_11"
-          data-name="7_line_11"
-          class="cls-3"
-          d="M1177.85,1069.47c52.65,2.08,37.4-.69,127,86.46"
-          transform="translate(-234.39 -341.08)"/>
-        <line
-          id="_7_line_10"
-          data-name="7_line_10"
-          class="cls-3"
-          x1="742.76"
-          y1="728.84"
-          x2="943.46"
-          y2="728.39"/>
-        <line
-          id="_7_line_9"
-          data-name="7_line_9"
-          class="cls-3"
-          x1="503.56"
-          y1="728.67"
-          x2="742.76"
-          y2="728.84"/>
-        <path
-          id="_7_line_8"
-          data-name="7_line_8"
-          class="cls-3"
-          d="M628.55,1010.36c68.35,72.26,59.43,58.34,109.4,59.39"
-          transform="translate(-234.39 -341.08)"/>
-        <line
-          id="_7_line_7"
-          data-name="7_line_7"
-          class="cls-3"
-          x1="313.93"
-          y1="589.82"
-          x2="394.16"
-          y2="669.28"/>
-        <path
-          id="_7_line_6"
-          data-name="7_line_6"
-          class="cls-3"
-          d="M462.75,810.83c1.62,45.69-1.56,30.69,85.57,120.07"
-          transform="translate(-234.39 -341.08)"/>
-        <line
-          id="_7_line_5"
-          data-name="7_line_5"
-          class="cls-3"
-          x1="228.36"
-          y1="412.08"
-          x2="228.36"
-          y2="469.74"/>
-        <line
-          id="_7_line_4"
-          data-name="7_line_4"
-          class="cls-3"
-          x1="227.98"
-          y1="357.78"
-          x2="228.36"
-          y2="412.08"/>
-        <line
-          id="_7_line_3"
-          data-name="7_line_3"
-          class="cls-3"
-          x1="227.99"
-          y1="297.91"
-          x2="227.98"
-          y2="357.78"/>
-        <line
-          id="_7_line_2"
-          data-name="7_line_2"
-          class="cls-3"
-          x1="227.98"
-          y1="216.74"
-          x2="227.99"
-          y2="297.91"/>
-        <line
-          id="_7_line_1"
-          data-name="7_line_1"
-          class="cls-3"
-          x1="227.98"
-          y1="177.36"
-          x2="227.98"
-          y2="216.74"/>
-      </g>
-      <g
-        id="_2_line"
-        data-name="2_line">
-        <path
-          id="_2_line-2"
-          data-name="2_line"
-          class="cls-5"
-          d="M679.86,344.85l-.07,56.6v39.2l.07,39.48V760.49c0,72.2-14.52,47.39,121.31,183.22l72.34,72.34,84.36,84.37c133.16,133.15,118.68,116.32,147.2,120.15,34.25,4.6,11.39-15.6,152.62,130.19,74.13,76.51,66,54.08,69.47,114.12s0,185.11,0,185.11v512.13c0,66.16,1.12,56.45,109.56,56.45h328.72"
-          transform="translate(-234.39 -341.08)"/>
-      </g>
-      <g
-        id="_10_line"
-        data-name="10_line">
-        <path
-          id="_10_line-2"
-          data-name="10_line"
-          class="cls-7"
-          d="M897.52,344.32v41.93c0,55.09-5,38,92.7,135.75l65,65,23.6,23.59c22.37,22.37,22,32.35,22,39.73V909.17c0,76.75,48.22,62.37,165.44,62.37,45,1.55,36.57-5.61,151.65,115.37l131.16,128.21c30.33,23.45,18.66,63.4,22.52,219.83v767.4"
-          transform="translate(-234.39 -341.08)"/>
-      </g>
-      <g
-        id="_9_line"
-        data-name="9_line">
-        <path
-          id="_9_line-2"
-          data-name="9_line"
-          class="cls-9"
-          d="M1205.47,344.1v37.78c.11,33.52-16,42.62-16,42.62l-59.36,59.36-75.64,75.64L1003,611c-58.53,50.3-52.59,59.13-52.7,77.12v62.63c0,59.87,3,51.13,26.54,74.73l95.39,95.39c28.06,19.6,25.73,77.67-.06,103.35-20.57,20.49-28.63,26.77-75.55,73.69-92.73,92.73-91,71.69-85.43,184.52,2.66,54.32-6.9,25.36,109,144.48,69.84,69.85,64.41,66.7,64.41,88.08v695.86"
-          transform="translate(-234.39 -341.08)"/>
-        <g
-          id="_6_line"
-          data-name="6_line">
-          <path
-            id="_6_line-2"
-            data-name="6_line"
-            class="cls-11"
-            d="M1337.16,414.13h0v247.4c0,35.47,2.2,27-44.9,74.07l-54.4,54.24c-53.48,53.49-44.11,44.06-44.11,104.8s-6.5,44.46,55.42,106.37c30.84,30.85,23.7,12.14,24,154,.42,171,13.77,147.69-49.7,211.16L1149,1440.71c-27.64,27.63-30.22,22.58-63.37,23.49s-36,0-36,0c-36.09,0-38.94,7.3-47.72,16.08l-58.06,58.06-55.8,55.8c-60,60-46,28.92-46,125.78v419.56"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_1_line"
-          data-name="1_line">
-          <path
-            id="_1_line-2"
-            data-name="1_line"
-            class="cls-13"
-            d="M1529.62,656.74v57.94c0,41.8,0,43.42-13.37,56.76l-48.74,48.73-34.68,34.69-49.45,49.44-52.1,52.1-45.36,45.37-95.87,95.86-96,96L970.72,1317c-57.11,57.11-46.31,50.85-82.24,50.85-87.75,0-79.6-3.24-115.13,32.29l-75.94,76-82.58,82.57c-23.11,23.11-17.14,31.56-17.14,137.16v502.56"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_3_line"
-          data-name="3_line">
-          <path
-            id="_3_line-2"
-            data-name="3_line"
-            class="cls-15"
-            d="M1722.52,678.68V785.46c-1.22,35.57,3,30.2-46.07,79.23l-79.86,79.86-35.91,35.91L1529.19,1012l-72,72-141.62,141.62c-15.07,16.16-25.73,23.54-50.07,23.54H668.65c-45.1,0-45.12-4.33-71.31,21.86s-31.33,29.66-60.09,58.42c-9.25,9.25-23.44,16.3-48.29,16.3H283.42c-31.16,0-38.27-19.83-38.27-32s-.2-179.17-.2-179.17V520.38"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_8_line"
-          data-name="8_line">
-          <path
-            id="_8_line-2"
-            data-name="8_line"
-            class="cls-17"
-            d="M1813.43,978.78l-30.66,30.66-29,29-53.67,53.68-36.63,36.62-112.75,112.76-89.74,89.74c-48.37,48.36-43.39,55.33-100,55.33H1244.48"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_11_line"
-          data-name="11_line">
-          <path
-            id="_11_line-2"
-            data-name="11_line"
-            class="cls-19"
-            d="M977.72,750.58c-35.31,35.3-35.81,35.86-64.74,35.86H705.93c-42.18,0-33.56-8.58-89.13,47l-66,66.05c-57.86,60.63-67.37,41-86.71,187.93-1,7.33-6.26,77.38,30.87,81"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_8a_line"
-          data-name="8a_line">
-          <path
-            id="_8a_line-2"
-            data-name="8a_line"
-            class="cls-21"
-            d="M974.52,746.84c-35.31,35.31-34.15,34.22-63.09,34.22h-207c-42.18,0-36.56-5.59-92.13,50l-66,66.05c-57.86,60.63-67.37,41-86.71,187.93-1,7.33.31,14.7-2.25,97.07-.94,30.15-4.17,28.37-20.31,46.07,0,0-34.3,33.67-58.24,57.61s-23.43,29.67-23.43,78.52v702.38"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_4_line"
-          data-name="4_line">
-          <path
-            id="_4_line_2"
-            data-name="4_line_2"
-            class="cls-23"
-            d="M950.65,1282.48l-72.38-72.39c-17.64-17.64-23.24-14.48-76.82-14.48H416.55"
-            transform="translate(-234.39 -341.08)"/>
-          <path
-            id="_4_line_1"
-            data-name="4_line_1"
-            class="cls-23"
-            d="M666.73,1195.61c-87,0-71.1,2.87-110.5,42.27S508.68,1286,487.29,1286h-176c-40.54,0-42.68-6.46-42.68-38.35V970.47"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="_12_line"
-          data-name="12_line">
-          <path
-            id="_12_line-2"
-            data-name="12_line"
-            class="cls-25"
-            d="M860.6,2156.05h168.62c28.45,0,27.37,20.25,27.37,20.25v96.13c0,21.53-23.55,22.2-23.55,22.2H617"
-            transform="translate(-234.39 -341.08)"/>
-        </g>
-        <g
-          id="stations"
-          data-name="stations">
-          <station-mark
-            v-for="station in stationsGraph"
-            :stationData="getStationData(station.id)"
-            :key="station.id"
-            @click.native="handleClick(station.id)"/>
-        </g>
-    </g></svg>
+    </svg>
   </div>
 </template>
 
@@ -317,12 +31,14 @@ import moscowGraph from '../mapsdata/moscow-graph';
 import moscowRenderData from '../mapsdata/render/moscow-render-data';
 import pathFinder from '../utils/pathFinder';
 import StationMark from './station-mark/StationMark.vue';
+import MoscowLines from './lines/MoscowLines.vue';
 
 export default {
     name : 'MoscowMap',
 
     components : {
         StationMark,
+        MoscowLines,
     },
 
     data() {
@@ -337,7 +53,6 @@ export default {
 
     methods : {
         pathFinder,
-
         getStationsGraph() {
             const stationsDataMapper = (node) => {
                 const stationRenderData = moscowRenderData.find( point => point.id === node.id );
@@ -402,61 +117,12 @@ export default {
 </script>
 
 <style scoped>
-.cls-1, .cls-11, .cls-13, .cls-15, .cls-17, .cls-19, .cls-21, .cls-23, .cls-25, .cls-3, .cls-5, .cls-7, .cls-9 {
-fill: none;
-}
-
 .cls-1, .cls-2 {
-stroke: #8a4f35;
-}
-
-.cls-1, .cls-10, .cls-11, .cls-12, .cls-13, .cls-14, .cls-15, .cls-16, .cls-17, .cls-18, .cls-19, .cls-2, .cls-20, .cls-21, .cls-22, .cls-23, .cls-24, .cls-25, .cls-26, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7, .cls-8, .cls-9 {
-stroke-miterlimit: 10;
-stroke-width: 5px;
-}
-
-.cls-3, .cls-4 {
-stroke: #8e489c;
-}
-
-.cls-5, .cls-6 {
-stroke: #48b85e;
-}
-
-.cls-7, .cls-8 {
-stroke: #b4d344;
-}
-
-.cls-10, .cls-9 {
-stroke: #a1a2a3;
-}
-
-.cls-11, .cls-12 {
-stroke: #f58220;
-}
-
-.cls-13, .cls-14 {
-stroke: #da2128;
-}
-
-.cls-15, .cls-16 {
-stroke: #0078bf;
-}
-
-.cls-17, .cls-18, .cls-21, .cls-22 {
-stroke: #fedd00;
-}
-
-.cls-19, .cls-20 {
-stroke: #79cdcd;
-}
-
-.cls-23, .cls-24 {
-stroke: #0db0e6;
-}
-
-.cls-25, .cls-26 {
-stroke: #acbfe3;
+    stroke-miterlimit: 10;
+    stroke-width: 3px;
+    fill: #fff;
+    stroke : red;
+    cursor: pointer;
 }
 
 .added-station {
